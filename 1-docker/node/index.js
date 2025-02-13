0938a0027c1dfba3c62f3c097a8d5442bac4d6b2
kleberaluizio <kleberaluizio@gmail.com>
2025-02-13 18:39:19 -0300

    Finish watching classes about docker

--- 1-docker/node/index.js
+++ 1-docker/node/index.js
@@ -2,8 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = {
+    host: "db",
+    user: "root",
+    password: "root",
+    database: "nodedb"
+};
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const insertPeople =  `INSERT INTO people (name) VALUES ('Kleber')`
+
+connection.query(insertPeople)
+connection.end
+
 app.get('/', (req, res) =>{
-    res.send('<h1> Node js seems pretty simple and easy </h1>')
+    res.send('<h1> 20250211 - Learning how to use docker compose </h1>')
 })
 
 app.listen(port,()=>{
